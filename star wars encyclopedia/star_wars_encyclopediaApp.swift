c9cf47880e7308ec850cab4401112c69ca36677c
Diego Sánchez Escribano <sanchez.escribano.diego@gmail.com>
2023-12-01 20:06:52 +0100

    Initial Commit

---
+++ star wars encyclopedia/star_wars_encyclopediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  star_wars_encyclopediaApp.swift
+//  star wars encyclopedia
+//
+//  Created by Diego Sánchez on 1/12/23.
+//
+
+import SwiftUI
+
+@main
+struct star_wars_encyclopediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ star wars encyclopediaUITests/star_wars_encyclopediaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  star_wars_encyclopediaUITestsLaunchTests.swift
+//  star wars encyclopediaUITests
+//
+//  Created by Diego Sánchez on 1/12/23.
+//
+
+import XCTest
+
+final class star_wars_encyclopediaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
