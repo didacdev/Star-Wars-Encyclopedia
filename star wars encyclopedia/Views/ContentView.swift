8b3470ced7011707230d7cdc5b877f00f0f65f28
Diego Sánchez Escribano <sanchez.escribano.diego@gmail.com>
2023-12-02 19:55:27 +0100

    Starting project

---
+++ star wars encyclopedia/Views/ContentView.swift
@@ -0,0 +1,29 @@
+
+//
+//  ContentView.swift
+//  star wars encyclopedia
+//
+//  Created by Diego Sánchez on 1/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Color.background
+            .edgesIgnoringSafeArea(.all)
+    }
+    
+//    init() {
+//        for familiy in UIFont.familyNames {
+//            print(familiy)
+//            for font in UIFont.fontNames(forFamilyName: familiy) {
+//                print("-- \(font)")
+//            }
+//        }
+//    }
+}
+
+#Preview {
+    ContentView()
+}
